b34b8a365c32f72fa2c1407301d4828414f3f599
Shashvat Kedia <shashvat51@gmail.com>
2017-09-10 22:15:08 +0530

    Add files via upload

--- app/src/main/java/com/example/shashvatkedia/movieapi/CustomGridAdapter.java
+++ app/src/main/java/com/example/shashvatkedia/movieapi/CustomGridAdapter.java
@@ -2,7 +2,9 @@ package com.example.shashvatkedia.movieapi;
 
 import android.content.Context;
 import android.graphics.Movie;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 import static android.R.attr.id;
@@ -33,6 +40,9 @@ public class CustomGridAdapter extends ArrayAdapter<MovieInfo>{
         MovieInfo mov=getItem(position);
         TextView name=(TextView) convertView.findViewById(R.id.movie_name);
         name.setText(mov.getName());
+        ImageView poster=(ImageView) convertView.findViewById(R.id.poster_view);
+        String url="http://image.tmdb.org/t/p/w185"+mov.getPath();
+        Picasso.with(getContext()).load(url).into(poster);
         return convertView;
     }
 
