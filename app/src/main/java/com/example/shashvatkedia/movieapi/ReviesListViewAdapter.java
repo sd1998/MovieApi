7be665922d4de2089b75fc7c0fac80fb49fa5ce1
sd1998 <shashvat51@gmail.com>
2017-10-28 21:33:18 +0530

    Initial Commit

--- app/src/main/java/com/example/shashvatkedia/movieapi/ReviesListViewAdapter.java
+++ app/src/main/java/com/example/shashvatkedia/movieapi/ReviesListViewAdapter.java
@@ -1,11 +1,49 @@
 package com.example.shashvatkedia.movieapi;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by shashvatkedia on 28/10/17.
  */
 
 public class ReviesListViewAdapter extends ArrayAdapter<Reviews> {
-    
+    Context con;
+    public ReviesListViewAdapter(Context context, ArrayList<Reviews> reviewsList){
+        super(context,0,reviewsList);
+        con = context;
+    }
+    public static class ViewHolder{
+        TextView authorView;
+        TextView contentVIew;
+    }
+
+    @Override
+    public View getView(int position,View convertView,ViewGroup parent){
+        ViewHolder viewHolder;
+        View view;
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.reviewslistcustomcomponent,parent,false);
+            viewHolder.authorView = (TextView) convertView.findViewById(R.id.authorTextView);
+            viewHolder.contentVIew = (TextView) convertView.findViewById(R.id.contentTextView);
+            view = convertView;
+            convertView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
+            view = convertView;
+        }
+        final Reviews review = getItem(position);
+        viewHolder.contentVIew.setText(review.getReview());
+        viewHolder.authorView.setText(review.getReviewAuthor());
+        return convertView;
+    }
 }
